227407987e9afb78828372e7a89de9bc35be03b1
Alex <djsasha777@mail.ru>
2025-07-14 19:06:49 +0300

    edit

--- OPENWRT/nginx/nginx-allconf.sh
+++ OPENWRT/nginx/nginx-allconf.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/djsasha777/hardware.git
 mv hardware/OPENWRT/nginx/nginx.conf /etc/nginx/nginx.conf
 mv hardware/OPENWRT/startpage/info.html /etc/nginx/info.html
-mv hardware/OPENWRT/startpage/home.html /etc/nginx/home.html
+mv hardware/OPENWRT/home/home.html /etc/nginx/home.html
 rm -rf hardware
 chmod 644 /etc/nginx/nginx.conf
 chmod 664 /etc/nginx/info.html
