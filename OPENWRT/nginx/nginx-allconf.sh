c5219c5b11d28a9ce7a358a6ff144e824374f303
Alex <djsasha777@mail.ru>
2025-07-14 18:58:35 +0300

    up

--- OPENWRT/nginx/nginx-allconf.sh
+++ OPENWRT/nginx/nginx-allconf.sh
@@ -1,8 +1,10 @@
 git clone https://github.com/djsasha777/hardware.git
 mv hardware/OPENWRT/nginx/nginx.conf /etc/nginx/nginx.conf
 mv hardware/OPENWRT/startpage/info.html /etc/nginx/info.html
+mv hardware/OPENWRT/startpage/home.html /etc/nginx/home.html
 rm -rf hardware
 chmod 644 /etc/nginx/nginx.conf
 chmod 664 /etc/nginx/info.html
+chmod 664 /etc/nginx/home.html
 service nginx restart
 echo "files copied and nginx restarted."
