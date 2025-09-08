a365ef26f80b57405118181cdefd1680074cda56
Alex <djsasha777@mail.ru>
2025-09-08 21:36:40 +0300

    up

--- OPENWRT/nginx/nginx-allconf.sh
+++ OPENWRT/nginx/nginx-allconf.sh
@@ -1,10 +1,8 @@
 git clone https://github.com/djsasha777/hardware.git
-mv hardware/OPENWRT/nginx/nginx.conf /etc/nginx/nginx.conf
-mv hardware/OPENWRT/startpage/info.html /etc/nginx/info.html
-mv hardware/OPENWRT/home/home.html /etc/nginx/home.html
-mv hardware/OPENWRT/nginx/nginx-allconf.sh /root/run.sh
+mv hardware/OPENWRT/nginx/* /etc/nginx/
+mv /etc/nginx/nginx-allconf.sh /root/run.sh
 rm -rf hardware
-chmod 777 /root/run.sh
+chmod +x /root/run.sh
 chmod 644 /etc/nginx/nginx.conf
 chmod 664 /etc/nginx/info.html
 chmod 664 /etc/nginx/home.html
