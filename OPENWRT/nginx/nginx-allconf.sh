a3aed91ce46cf0dc84c6488836bac5ef20e67e73
Alex <djsasha777@mail.ru>
2025-09-09 12:23:01 +0300

    r

--- OPENWRT/nginx/nginx-allconf.sh
+++ OPENWRT/nginx/nginx-allconf.sh
@@ -3,6 +3,6 @@ cp -rf hardware/OPENWRT/nginx/* /etc/nginx/
 mv /etc/nginx/nginx-allconf.sh /root/run.sh
 rm -rf hardware
 chmod +x /root/run.sh
-chmod 664 -R /etc/nginx
+chmod 777 -R /etc/nginx
 service nginx restart
 echo "files copied and nginx restarted."
