ae5392d23af0d759c91ed53d055aead850bce806
Alex <djsasha777@mail.ru>
2025-09-09 13:02:18 +0300

    up

--- OPENWRT/nginx/nginx-allconf.sh
+++ OPENWRT/nginx/nginx-allconf.sh
@@ -5,7 +5,6 @@ rm -rf hardware
 chmod +x /root/run.sh
 chmod 644 -R /etc/nginx
 chmod 755 /etc/nginx
-chmod 777 -R /etc/nginx/player
 chmod 755 /etc/nginx/player
 service nginx restart
 echo "files copied and nginx restarted."
