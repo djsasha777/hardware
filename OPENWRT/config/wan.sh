0eca132fd9b1b60a0c227a991a1edc70e3a0583c
Alex <djsasha777@mail.ru>
2025-09-13 17:18:40 +0300

    ed

--- OPENWRT/config/wan.sh
+++ OPENWRT/config/wan.sh
@@ -14,11 +14,11 @@ service firewall restart
 
 
 
-or add to /etc/config/firewall
+# or add to /etc/config/firewall
 
-config rule
-        option name 'Allow-Admin'
-        option src 'wan'
-        option proto 'tcp'
-        option dest_port '22 80 443 8383'
-        option target 'ACCEPT'
+# config rule
+#         option name 'Allow-Admin'
+#         option src 'wan'
+#         option proto 'tcp'
+#         option dest_port '22 80 443 8383'
+#         option target 'ACCEPT'
